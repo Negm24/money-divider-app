6058d4b0dc2167d5df8054481935838c4fab33ba
Negm <youssefkhalednegm24@gmail.com>
2024-07-28 14:48:58 +0300

    Added Google Analytics tracking code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Main from './components/Main';
-import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
+// import ReactDOM from 'react-dom';
+// import ReactGA from 'react-ga';
 
 // Initialize Google Analytics
-ReactGA.initialize('G-0V36VV6LK1');
+// ReactGA.initialize('G-0V36VV6LK1');
 
-// Track the initial page load
-ReactGA.pageview(window.location.pathname + window.location.search);
+// // Track the initial page load
+// ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 function App() {
   return (
